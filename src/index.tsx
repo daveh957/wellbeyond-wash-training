3f5238f18c7d2b2d8327cefb65e44ef69cf09ddc
Dave Hart <dave@greatfeats.com>
2020-05-02 15:00:58 -0500

    use gravatar

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 // import i18n (needs to be bundled ;))
 import './i18n';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.register();
